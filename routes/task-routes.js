fcc392c744a61b886fc73f0d2d9765f2881ef93f
Michael Clare <michaelclare@michaels-mbp.home>
2020-03-23 14:25:04 +0000

    Adding timestamps and run durations. Testing passed

--- routes/task-routes.js
+++ routes/task-routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Task } = require("../models/task");
+const { Task,Instance } = require("../models/task");
 const router = new express.Router();
 const { auth } = require("../middleware/auth");
 
@@ -30,25 +30,20 @@ router.get("/tasks", auth, async (req, res) => {
 
 //Currently working on this
 router.patch("/tasks/:id", auth, async (req, res) => {
-  console.log("test")
-  console.log(req.body)
   const updates = Object.keys(req.body);
   const allowedUpdates = ["totalTime"];
   const isValidOperation = updates.every(update =>
     allowedUpdates.includes(update)
   );
-  console.log(isValidOperation)
   if (!isValidOperation) {
     return res.status(400).send({ error: "Invalid update" });
   }
-  console.log("test")
   try {
     const task = await Task.findOne({
       _id: req.params.id,
       owner: req.user._id
     });
     updates.forEach(update => (task[update] = req.body[update]));
-    console.log(task)
 
     await task.save();
 
@@ -62,6 +57,36 @@ router.patch("/tasks/:id", auth, async (req, res) => {
   }
 });
 
+router.patch("/tasks/instance/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["timeStarted","timeRan"];
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid update" });
+  }
+  const instance = new Instance({timeStarted:req.body.timeStarted, timeRan:req.body.timeRan})
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id
+    });
+    task.runTime.push(instance)
+    console.log(task)
+    await task.save();
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(202).send(task);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+//Needs reviewing
 router.delete("/tasks/:id", auth, async (req, res) => {
   try {
     const task = await Task.findByIdAndDelete({
